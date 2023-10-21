741323cbbb1d631d26a4fc50114a832d9a89a508
lennart Czardybon <lennczar@gmail.com>
2023-10-21 17:27:05 +0200

    replace logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.svg";
+import logo from "../img/logo.png";
 
 const Navbar = () => {
     const [isActive, setIsActive] = useState(false);
